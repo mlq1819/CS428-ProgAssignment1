167dbf1974a5f40ffe596d7df674c34eee4d3bdb
Michael Quinn <mquinn5@binghamton.edu>
2020-02-27 17:31:03 -0500

    removed print server address

--- server.cpp
+++ server.cpp
@@ -36,8 +36,6 @@ int main() {
 	// Bind the socket with the server address 
 	bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr));
 	
-	std::cout << servaddr.sin_family << std::endl;
-	
 	// random generator
 	srand(time(0));
 
