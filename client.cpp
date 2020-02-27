5d73b9da89aafd3ee9c09842b6b4b07bdf9c5923
Michael Quinn <mquinn5@binghamton.edu>
2020-02-27 16:54:46 -0500

    added client

---
+++ client.cpp
@@ -0,0 +1,16 @@
+// UDP Client
+
+// Must have the UDP Pinger Server running before you can run this client
+
+#include <iostream>
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <time.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+
+#define PORT	 12000
+
